99b8cae58933f718013ea37083c1f8ae98463be6
Jordan_QD <qiaobinghe@outlook.com>
2024-01-08 00:47:01 +0800

    Update fuckWechat.sh

--- fuckWechat.sh
+++ fuckWechat.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 目标监控目录
-WATCHED_DIR="/Users/jordan_qd/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/a04c27f1307b84a058a7ff33f7897785/Message/MessageTemp"
+WATCHED_DIR="~/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/a04c27f1s7b84a0o8a72ff33f787785/Message/MessageTemp"
 
 # 文件被移动到的目录
-DESTINATION_DIR="/Users/jordan_qd/Downloads"
+DESTINATION_DIR="~/Downloads"
 
 # 开始递归监控目录
 fswatch -o "$WATCHED_DIR" | while read f; do
